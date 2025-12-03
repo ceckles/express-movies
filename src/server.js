e200e9bff05c2e876f9cd5dc485c0ffdc6e85e88
Chad <chad1020@gmail.com>
2025-12-03 03:27:55 -0600

    fix: host fix

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const movieRoutes = require('./routes/movieRoutes');
 const watchlistRoutes = require('./routes/watchlistRoutes');
 
 //ENV Variables
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 
 
 config();
